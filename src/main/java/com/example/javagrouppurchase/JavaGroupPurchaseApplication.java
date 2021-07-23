612df7711d17027dd4117bc7b7da613f9dbd0619
Christophe Thibaut <cthibaut@octo.com>
2021-07-23 13:23:09 +0200

    Initial commit

---
+++ src/main/java/com/example/javagrouppurchase/JavaGroupPurchaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.javagrouppurchase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGroupPurchaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaGroupPurchaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javagrouppurchase/JavaGroupPurchaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javagrouppurchase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaGroupPurchaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
